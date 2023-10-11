abfb75636329c4f40b39db1848214af5beacebc9
Fahmi Zulkarnain <73563319+zulkou@users.noreply.github.com>
2023-10-11 17:49:06 +0700

    add "add a button to an app"

--- app/src/main/java/com/example/juaraandroid/MainActivity.kt
+++ app/src/main/java/com/example/juaraandroid/MainActivity.kt
@@ -9,15 +9,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,16 +44,85 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JuaraAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ("Android")
-                }
+                LemonadeApp()
             }
         }
     }
+}
+
+@Composable
+fun LemonSqueeze(modifier: Modifier = Modifier) {
+    var stage by remember { mutableStateOf(1) }
+    val imageResource = when(stage) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val textResource = when(stage) {
+        1 -> R.string.lemon1
+        2 -> R.string.lemon2
+        3 -> R.string.lemon3
+        else -> R.string.lemon4
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { if(stage!=4){stage++}else{stage = 1} }) {
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = stage.toString()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = stringResource(id = textResource))
+    }
+}
+
+@Preview
+@Composable
+fun LemonadeApp() {
+    LemonSqueeze(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
+
+//@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
 }
 
 @Composable
@@ -295,25 +372,24 @@ fun BusinessCard(fullName: String, title: String, phone: String, handle: String,
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JuaraAndroidTheme {
-        //        GreetingImage("Happy Birthday Jack!", "From Zulkou")
-//        ComposeArticle(
-//            stringResource(R.string.article_title),
-//            stringResource(R.string.article_subtext),
-//            stringResource(R.string.article_main)
-//        )
-//        TaskManager()
-//        ComposeQuadrant()
-        BusinessCard(
-            fullName = "Fahmi Zulkarnain",
-            title = "Android Developer",
-            phone = "1234567890",
-            handle = "@zulkou",
-            email = "zulkou@email.com"
-        )
-
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    JuaraAndroidTheme {
+//        //        GreetingImage("Happy Birthday Jack!", "From Zulkou")
+////        ComposeArticle(
+////            stringResource(R.string.article_title),
+////            stringResource(R.string.article_subtext),
+////            stringResource(R.string.article_main)
+////        )
+////        TaskManager()
+////        ComposeQuadrant()
+////        BusinessCard(
+////            fullName = "Fahmi Zulkarnain",
+////            title = "Android Developer",
+////            phone = "1234567890",
+////            handle = "@zulkou",
+////            email = "zulkou@email.com"
+////        )
+//    }
+//}
