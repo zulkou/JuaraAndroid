b4b3dcd07cad771525201168761f39bacbbf1aa8
Fahmi Zulkarnain <73563319+zulkou@users.noreply.github.com>
2023-10-13 21:06:57 +0700

    add "refactor files"

--- app/src/main/java/com/example/juaraandroid/MainActivity.kt
+++ app/src/main/java/com/example/juaraandroid/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.juaraandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,10 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,12 +28,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,352 +43,107 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JuaraAndroidTheme {
-                LemonadeApp()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArtSpaceLayout()
+                }
             }
         }
     }
 }
 
 @Composable
-fun LemonSqueeze(modifier: Modifier = Modifier) {
+fun ArtSpaceLayout() {
     var stage by remember { mutableStateOf(1) }
-    val imageResource = when(stage) {
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp, top = 32.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        ArtDisplay(
+            stage = stage,
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = { if(stage!=1){stage--}else{stage = 4} }) {
+                Text("Previous")
+            }
+            Button(
+                onClick = { if(stage!=4){stage++}else{stage = 1} },
+                ) {
+                Text("Next")
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtDisplay(
+//    @DrawableRes artImage: Int,
+//    @StringRes artTitle: Int,
+//    @StringRes artist: Int,
+    stage: Int,
+//    onStageChange: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val artImage = when(stage) {
         1 -> R.drawable.lemon_tree
         2 -> R.drawable.lemon_squeeze
         3 -> R.drawable.lemon_drink
         else -> R.drawable.lemon_restart
     }
-    val textResource = when(stage) {
+    val artTitle = when(stage) {
         1 -> R.string.lemon1
         2 -> R.string.lemon2
         3 -> R.string.lemon3
         else -> R.string.lemon4
     }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = { if(stage!=4){stage++}else{stage = 1} }) {
-            Image(
-                painter = painterResource(id = imageResource),
-                contentDescription = stage.toString()
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = stringResource(id = textResource))
+    val artist = when(stage) {
+        1 -> R.string.lemon1
+        2 -> R.string.lemon2
+        3 -> R.string.lemon3
+        else -> R.string.lemon4
     }
-}
-
-@Preview
-@Composable
-fun LemonadeApp() {
-    LemonSqueeze(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
-}
-
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when(result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = imageResource),
-            contentDescription = result.toString()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
-        }
-    }
-}
-
-//@Preview
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
-}
-
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp),
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-    }
-}
-
-@Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-
-    Box {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        )
-    }
-}
-
-@Composable
-fun ComposeArticle(title: String, subtext: String, main: String, modifier: Modifier = Modifier) {
-    val banner = painterResource(R.drawable.bg_compose_background)
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Image(
-            painter = banner,
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-        )
-        Text(
-            text = title,
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(16.dp)
-        )
-        Text(
-            text = subtext,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp)
-        )
-        Text(
-            text = main,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier
-                .padding(16.dp)
-        )
-    }
-}
-
-@Composable
-fun TaskManager(modifier: Modifier = Modifier) {
-    val complete = painterResource(R.drawable.ic_task_completed)
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = complete,
-            contentDescription = null,
-        )
-        Text(
-            text = stringResource(R.string.notify_title),
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(top = 24.dp, bottom = 8.dp)
-        )
-        Text(
-            text = stringResource(R.string.notify_description),
-            fontSize = 16.sp
-        )
-    }
-}
-
-@Composable
-fun ComposeQuadrant(modifier: Modifier = Modifier) {
-    Column(Modifier.fillMaxWidth()) {
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(R.string.first_title),
-                description = stringResource(R.string.first_description),
-                backgroundColor = Color(0xFFEADDFF),
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = stringResource(R.string.second_title),
-                description = stringResource(R.string.second_description),
-                backgroundColor = Color(0xFFD0BCFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(R.string.third_title),
-                description = stringResource(R.string.third_description),
-                backgroundColor = Color(0xFFB69DF8),
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = stringResource(R.string.fourth_title),
-                description = stringResource(R.string.fourth_description),
-                backgroundColor = Color(0xFFF6EDFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
-    }
-}
-
-@Composable
-private fun ComposableInfoCard(title: String, description: String, backgroundColor: Color, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            modifier = Modifier.padding(bottom = 16.dp),
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = description,
-            textAlign = TextAlign.Justify
-        )
-    }
-}
-
-@Composable
-fun BusinessCard(fullName: String, title: String, phone: String, handle: String, email: String, modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+            .shadow(elevation = 10.dp, shape = RectangleShape)
     ) {
-        Column (
-            modifier = Modifier
-                .align(Alignment.Center)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val logo = painterResource(R.drawable.android_logo)
+        Image(
+                painter = painterResource(id = artImage),
+        contentDescription = null,
+        modifier = modifier
+            .padding(20.dp)
 
-            Image(
-                painter = logo,
-                contentDescription = null,
-                modifier = Modifier
-                    .width(150.dp)
-            )
-            Text(
-                text = fullName,
-                fontSize = 32.sp,
-            )
-            Text(
-                text = title,
-                fontSize = 16.sp,
-            )
-        }
-        Column(
-            modifier = Modifier
-                .align(Alignment.BottomCenter),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.Start
-        ) {
-            Row(
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                val phoneIcon = painterResource(R.drawable.ic_phone)
-
-                Icon(
-                    painter = phoneIcon,
-                    contentDescription = null
-                )
-                Text(
-                    text = phone,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
-            }
-            Row(
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                val handleIcon = painterResource(R.drawable.ic_handle)
-
-                Icon(
-                    painter = handleIcon,
-                    contentDescription = null
-                )
-                Text(
-                    text = handle,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
-            }
-            Row(
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                val emailIcon = painterResource(R.drawable.ic_email)
-
-                Icon(
-                    painter = emailIcon,
-                    contentDescription = null
-                )
-                Text(
-                    text = email,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
-            }
-        }
+        )
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    Column {
+        Text(
+            text = stringResource(id = artTitle),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Thin
+        )
+        Text(
+            text = stringResource(id = artist),
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    JuaraAndroidTheme {
-//        //        GreetingImage("Happy Birthday Jack!", "From Zulkou")
-////        ComposeArticle(
-////            stringResource(R.string.article_title),
-////            stringResource(R.string.article_subtext),
-////            stringResource(R.string.article_main)
-////        )
-////        TaskManager()
-////        ComposeQuadrant()
-////        BusinessCard(
-////            fullName = "Fahmi Zulkarnain",
-////            title = "Android Developer",
-////            phone = "1234567890",
-////            handle = "@zulkou",
-////            email = "zulkou@email.com"
-////        )
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun ArtSpacePreview() {
+    JuaraAndroidTheme {
+        ArtSpaceLayout()
+    }
+}
+
